d7c25ba7c60ffc71e62c1e3d89e07b4fe90fb9e3
ryohtaro <ryohtarot@gmail.com>
2020-01-15 20:04:02 +0900

    fix typo

--- index.js
+++ index.js
@@ -14,12 +14,12 @@
  * @param {Number} armorPenetration 防御力貫通
  * @return {Number} ダメージ
  */
-functinon effectiveDamage(power, armor, armorPenetration) {
+function effectiveDamage(power, armor, armorPenetration) {
   let effectiveArmor = normalize(armor) - normalize(armorPenetration);
   effectiveArmor = effectiveArmor <= 0 ? 0 : effectiveArmor;
   const damageDecrease = effectiveArmor / (100 + effectiveArmor);
   return Math.round(normalize(power) * (1 - damageDecrease));
-}
+};
 
 /**
  * 異常な可能性のある入力値を正常値にする
